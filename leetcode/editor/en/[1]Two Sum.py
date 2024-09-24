554a3e6c6fec8f5dc1f899506128fad66a9296e5
Yucheng-An <yuc.an2019@gmail.com>
2024-09-24 10:43:27 -0700

    Auto-commit Python

---
+++ leetcode/editor/en/[1]Two Sum.py
@@ -0,0 +1,16 @@
+
+# leetcode submit region begin(Prohibit modification and deletion)
+from typing import List
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # 维护 val -> index 的映射
+        val_to_index = {}
+        for i in range(len(nums)):
+            # 查表，看看是否有能和 nums[i] 凑出 target 的元素
+            need = target - nums[i]
+            if need in val_to_index:
+                return [val_to_index[need], i]
+            # 存入 val -> index 的映射
+            val_to_index[nums[i]] = i
+        return []
+# leetcode submit region end(Prohibit modification and deletion)
