efa8957d8a21e867f795c446ac2124c8de970b09
Yucheng-An <yuc.an2019@gmail.com>
2024-09-26 10:51:39 -0700

    Commit Python

--- main.py
+++ main.py
@@ -3,4 +3,13 @@ from audioop import reverse
 print("Nice to meet you")
 t = [130291875,474356040,1,8]
 t.sort(reverse=True)
-
+def minTime(files, numCores, limit):
+    files.sort(reverse=True)
+    result = 0
+    for file in files:
+        if file >= numCores and limit != 0:
+            result = result + (file//numCores)
+            limit -= 1
+        else:
+            result = result + file
+    return result
