72e1cf786728a84d24d617743e0333fdc0d2ade4
Yucheng-An <yuc.an2019@gmail.com>
2024-09-26 20:36:50 -0700

    Commit Python

--- main.py
+++ main.py
@@ -24,6 +24,6 @@ for a0 in range(t):
     for i in range(1, n+1):
         if i % 3 == 0 or i % 5 == 0:
             count += i
-            print(count)
+            print(i)
     result.append(count)
 print(result)
