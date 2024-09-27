734fddc09121770b6692ab8f8322982dad1989fe
Yucheng-An <yuc.an2019@gmail.com>
2024-09-26 20:35:54 -0700

    Commit Python

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ for a0 in range(t):
     n = int(input().strip())
     count = 0
     print(n)
-    for i in range(1,n+1):
+    for i in range(1, n+1):
         if i % 3 == 0 or i % 5 == 0:
             count += i
             print(count)
