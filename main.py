89133f4f249f93be9bb6332b1ecab49d49941211
Yucheng-An <yuc.an2019@gmail.com>
2024-09-28 16:31:27 -0700

    Commit Python

--- main.py
+++ main.py
@@ -1,32 +1,3 @@
-from audioop import reverse
+def reverse_string(arguments):
+    return arguments[::-1]
 
-print("Nice to meet you")
-t = [130291875,474356040,1,8]
-t.sort(reverse=True)
-def minTime(files, numCores, limit):
-    files.sort(reverse=True)
-    result = 0
-    for file in files:
-        if file >= numCores and limit != 0:
-            result = result + (file//numCores)
-            limit -= 1
-        else:
-            result = result + file
-    return result
-print(minTime(t,5,3))
-
-def timeConversion(s):
-    hours = s[0] + s[1]
-    minutes = s[3] + s[4]
-    seconds = s[6] + s[7]
-    if s[8] + s[9] == "PM":
-        hours = int(hours) + 12
-        if hours == 24:
-            hours = 00
-        return str(hours)+ ":" + minutes + ":" + seconds
-    elif s[8] + s[9] == "AM":
-        if hours == "12":
-            hours == "00"
-        return hours + ":" + minutes + ":" + seconds
-
-print(timeConversion("12:45:54PM"))
