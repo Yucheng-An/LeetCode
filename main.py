a0926449ea14290805ba7c29098432cf0de66942
Yucheng-An <yuc.an2019@gmail.com>
2024-09-30 11:58:37 -0700

    Commit Python

--- main.py
+++ main.py
@@ -8,3 +8,13 @@ class Node:
         self.val = element
         self.next = None
         self.prev = None
+
+
+
+# 长度为 5 的数组
+arr = [1, 2, 3, 4, 5]
+i = 0
+# 模拟环形数组，这个循环永远不会结束
+while i < len(arr):
+    print(arr[i])
+    i = (i + 1) % len(arr)
