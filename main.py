a9e6d292230bc29fef63cf0d93315e90f3771a36
Yucheng-An <yuc.an2019@gmail.com>
2024-09-30 11:58:41 -0700

    Commit Python

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-
 class ListNode:
     def __init__(self, x):
         self.val = x
         self.next = None
+
+
 class Node:
     def __init__(self, prev, element, next):
         self.val = element
         self.next = None
         self.prev = None
-
 
 
 # 长度为 5 的数组
