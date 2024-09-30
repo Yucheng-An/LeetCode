2215bc2ddac76e69aee9fe1be577a9f166ee1bef
Yucheng-An <yuc.an2019@gmail.com>
2024-09-30 11:47:21 -0700

    Commit Python

--- main.py
+++ main.py
@@ -2,3 +2,8 @@ class ListNode:
     def __init__(self, x):
         self.val = x
         self.next = None
+class Node:
+    def __init__(self, prev, element, next):
+        self.val = element
+        self.next = next
+        self.prev = prev
