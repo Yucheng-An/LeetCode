6a19086954f9da1b8bcc4bd3cec06f9b6c1bcbf8
Yucheng-An <yuc.an2019@gmail.com>
2024-10-01 01:30:45 -0700

    Commit Python

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ arr = [100, 98, 23, 55, 89, 12]
 for index , value in enumerate(arr):
     arr[index]= (value, index)
 print(arr)
-print(pair)
 
 pair.sort(key=lambda x: x[1], reverse = True)
 print(pair)
