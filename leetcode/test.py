3adbb905ef323c4291da2907370f85ee9568f327
Yucheng-An <yuc.an2019@gmail.com>
2024-10-02 01:14:59 -0700

    Commit Python

--- leetcode/test.py
+++ leetcode/test.py
@@ -1,11 +1,10 @@
 citations = [3,0,6,1,5]
 citations.sort(reverse=True)
 print(citations)
-newlist =[]
+newlistnewlist =[]
 for i in range(1,len(citations)+1):
     newlist.append(i)
 print(newlist)
 for i in range(len(citations)):
     if citations[i] < newlist[i] and i > 1:
         return newlist[i-1]
-    
