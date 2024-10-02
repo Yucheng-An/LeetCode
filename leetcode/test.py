7f2c6b406af67f249c3b4ebb0b95c2f440fb05b1
Yucheng-An <yuc.an2019@gmail.com>
2024-10-02 13:59:16 -0700

    Commit Python

--- leetcode/test.py
+++ leetcode/test.py
@@ -1,29 +1,3 @@
 citations = [3, 1, 1]
 
-
-def hindex(citations):
-    if len(citations) == 1:
-        if citations[0] == 0:
-            return 0
-        else:
-            return 1
-    citations.sort(reverse=True)
-    ranklist = [1]
-    rank = 1
-    p = 1
-    while p < len(citations):
-        if citations[p] != citations[p - 1]:
-            rank += 1
-        ranklist.append(rank)
-        p += 1
-    print(citations)
-    print(ranklist)
-    for i in range(len(citations)):
-        if ranklist[i] == citations[i]:
-            return ranklist[i]
-        if citations[i] < ranklist[i]:
-            return ranklist[i-1]
-
-
-
-hindex(citations)
+for i, 
