b1a385ba75ac1afce95f75802f0ab8f6579ec1ef
Yucheng-An <yuc.an2019@gmail.com>
2024-10-02 16:46:31 -0700

    Commit Python

--- main.py
+++ main.py
@@ -62,3 +62,4 @@ input_list = [30, 20, 40, 10, 35, 50, 5]
 balanceTree = list2Tree(input_list, root=25, balance=False)
 unbalanceTree = list2Tree(input_list, balance=True)
 balanceTree.print()
+unbalanceTree.print()
