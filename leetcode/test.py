667edeadb2ddbe8c28c4e1af8df5b18b051576f1
Yucheng-An <yuc.an2019@gmail.com>
2024-10-01 19:35:09 -0700

    Commit Python

--- leetcode/test.py
+++ leetcode/test.py
@@ -3,7 +3,7 @@ def arrayRankTransform(arr):
         arr[index] = (value, index, 0)
     arr.sort(key=lambda x: x[0])
     for i in range(len(arr)):
-        arr[i]= (arr[i])
+        arr[i]= (arr[i][0],arr[i][1])
 
 arr = [40, 10, 20, 30]
 arrayRankTransform(arr)
