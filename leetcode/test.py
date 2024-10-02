07f66b213868c4e8e298280335ba49dffb77fa17
Yucheng-An <yuc.an2019@gmail.com>
2024-10-01 19:30:28 -0700

    Commit Python

---
+++ leetcode/test.py
@@ -0,0 +1,10 @@
+def arrayRankTransform(self, arr: List[int]) -> List[int]:
+    for index, value in enumerate(arr):
+        arr[index] = (value,index,0)
+    arr.sort(key=lambda x : x[0])
+    pointer = 0
+    rank = 1
+    arr[0][3] = 1
+    while pointer < len(arr):
+        arr[pointer][2] =
+        
