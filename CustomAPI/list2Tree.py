d646c1b5187737fe562105ede266ce61aca5a7c1
Yucheng-An <yuc.an2019@gmail.com>
2024-10-02 16:58:12 -0700

    Commit Python

--- CustomAPI/list2Tree.py
+++ CustomAPI/list2Tree.py
@@ -50,13 +50,3 @@ def inorderTraversal(root, result):
         inorderTraversal(root.left, result)
         result.append(root.val)
         inorderTraversal(root.right, result)
-
-
-
-
-input_list = [30, 20, 40, 10, 35, 50, 5]
-
-balanceTree = list2Tree(input_list, root=25, balance=False)
-unbalanceTree = list2Tree(input_list, balance=True)
-balanceTree.printTree()
-unbalanceTree.printTree()
