5322f2bd429169183bfc3c8da0f394c8ada4f398
Yucheng-An <yuc.an2019@gmail.com>
2024-10-01 19:37:15 -0700

    Commit Python

--- leetcode/test.py
+++ leetcode/test.py
@@ -3,7 +3,7 @@ def arrayRankTransform(arr):
         arr[index] = (value, index, 0)
     arr.sort(key=lambda x: x[0])
     rank = 1
-    arr[1]
+    arr[1] = (arr[0][0],arr[0][1],rank)
     for i in range(1,len(arr)):
         if arr[i]
             arr[i]= (arr[i][0],arr[i][1],rank)
