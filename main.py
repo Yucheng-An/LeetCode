ff4ca84a4e945b00815dc27b849e840edcde8c59
Yucheng-An <yuc.an2019@gmail.com>
2024-10-02 16:43:35 -0700

    Commit Python

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ def inorder_traversal(root, result):
         inorder_traversal(root.right, result)
 
 
-def list_to_tree(inputList: list, root_node=None, balance=False):
+def list_to_tree(inputList: list, root=None, balance=False):
     if not inputList:
         raise ValueError("The input list cannot be empty.")
 
@@ -50,18 +50,18 @@ def list_to_tree(inputList: list, root_node=None, balance=False):
         inputList = sorted(inputList)  # Sorting for balanced tree
         root = create_balanced_tree(inputList)
     else:
-        # Step 2: Create a binary search tree from the list with a mandatory root_node
-        if root_node is None:
-            raise ValueError("root_node is required if balance=False.")
-        root = TreeNode(root_node)
+        # Step 2: Create a binary search tree from the list with a mandatory root
+        if root is None:
+            raise ValueError("root is required if balance=False.")
+        root = TreeNode(root)
         for item in inputList:
-            if item != root_node:
+            if item != root:
                 root = insert_node(root, item)
 
     return root
 
 
 input_list = [30, 20, 40, 10, 35, 50, 5]
-list_to_tree(input_list, root_node=25, balance=False)
+list_to_tree(input_list, root=25, balance=False)
 a = list_to_tree(input_list, balance=True)
 a.print()
