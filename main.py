2f8173fe08a5ca4ce1b6635cb935f8441f95ba5a
Yucheng-An <yuc.an2019@gmail.com>
2024-10-02 16:45:09 -0700

    Commit Python

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ def inorder_traversal(root, result):
 
 def list_to_tree(inputList: list, root=None, balance=False) -> TreeNode:
     if not inputList:
-        raise ValueError("The input list cannot Be Empty.")
+        raise ValueError("The input list cannot be empty.")
     if balance:
         inputList = sorted(inputList)
         root = create_balanced_tree(inputList)
