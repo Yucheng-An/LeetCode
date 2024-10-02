594b823333a73d816c6891d7e76cbf738f01744a
Yucheng-An <yuc.an2019@gmail.com>
2024-10-02 16:50:45 -0700

    Commit Python

--- main.py
+++ main.py
@@ -56,7 +56,6 @@ def inorderTraversal(root, result):
 
 input_list = [30, 20, 40, 10, 35, 50, 5]
 
-
 balanceTree = list2Tree(input_list, root=25, balance=False)
 unbalanceTree = list2Tree(input_list, balance=True)
 balanceTree.printTree()
