0274c59063d7ba91682bf3ee8d38ee5f12ac26df
Yucheng-An <yuc.an2019@gmail.com>
2024-10-02 16:56:28 -0700

    Commit Python

---
+++ CustomAPI/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="API",
+    version="0.1",
+    description="A package for string manipulation functions",
+    packages=find_packages(),
+    install_requires=[],  # No external dependencies
+    author="Ramis H., Yucheng A., Bex A.",
+    url="https://github.com/yourname/string_package",  # Update this with your repo
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
+
