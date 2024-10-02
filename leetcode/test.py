eacfcb2acdbcf36a5718a7f1dd61495c8a7bf88d
Yucheng-An <yuc.an2019@gmail.com>
2024-10-01 19:37:39 -0700

    Commit Python

--- leetcode/test.py
+++ leetcode/test.py
@@ -5,7 +5,8 @@ def arrayRankTransform(arr):
     rank = 1
     arr[1] = (arr[0][0],arr[0][1],rank)
     for i in range(1,len(arr)):
-        if arr[i][0] == arr            arr[i]= (arr[i][0],arr[i][1],rank)
+        if arr[i][0] == arr
+            arr[i]= (arr[i][0],arr[i][1],rank)
 
 arr = [40, 10, 20, 30]
 arrayRankTransform(arr)
