b040d9cfe9f95e25a49d33978eb590169e95f824
Yucheng-An <yuc.an2019@gmail.com>
2024-10-01 20:21:36 -0700

    Commit Python

--- leetcode/test.py
+++ leetcode/test.py
@@ -1,22 +1,4 @@
-def arrayRankTransform(arr):
-    for index, value in enumerate(arr):
-        arr[index] = (value, index, 0)
-    arr.sort(key=lambda x: x[0])
-    rank = 1
-    arr[0] = (arr[0][0], arr[0][1], rank)
-    print(arr)
-    for i in range(1, len(arr)):
-        if arr[i][0] == arr[i - 1][0]:
-            arr[i] = (arr[i][0], arr[i][1], rank)
-        else:
-            rank += 1
-            arr[i] = (arr[i][0], arr[i][1], rank)
-    arr.sort(key=lambda x: x[1])
-    res = []
-    for i in range(len(arr)):
-        res.append(arr[i][2])
-    return res
+s = "the sky is blue"
 
-
-arr = [40, 10, 20, 30]
-print(arrayRankTransform(arr))
+t = s.split()
+print(t)
