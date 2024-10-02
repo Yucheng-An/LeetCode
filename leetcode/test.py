76519aa006a590344dde2723daa54b442b37605e
Yucheng-An <yuc.an2019@gmail.com>
2024-10-02 13:43:17 -0700

    Commit Python

---
+++ leetcode/test.py
@@ -0,0 +1,14 @@
+if len(citations) == 1:
+    if citations[0]==0:
+        return 0
+    else:
+        return 1
+citations.sort(reverse=True)
+ranklist =[1]
+rank = 1
+p = 1
+while p <= len(citations):
+    if citations[p] != citations[p-1]:
+        rank += 1
+    ranklist.append(rank)
+    p+=1
