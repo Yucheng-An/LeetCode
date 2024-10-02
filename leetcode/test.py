c91b79f7b8f6fb409257f71ae257e3f1a8c46fa5
Yucheng-An <yuc.an2019@gmail.com>
2024-10-02 14:01:35 -0700

    Commit Python

--- leetcode/test.py
+++ leetcode/test.py
@@ -2,5 +2,6 @@ citations = [3, 1, 1]
 
 for i, citation in enumerate(citations):
     print(f"i is {i}")
-    print(f"rank is {rank}")
-    if 
+    print(f"rank is {citation}")
+    if citation < i + 1:
+        return i 
