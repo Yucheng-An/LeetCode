3ad7190115c83cba1613e743e58030f93d5772fa
Yucheng-An <yuc.an2019@gmail.com>
2024-10-02 23:42:38 -0700

    Commit Python

--- myLeetCode/303.py
+++ myLeetCode/303.py
@@ -3,7 +3,7 @@ class NumArray:
         self.nums = nums[0][0]
         result = ["null"]
         for i in range(1,len(nums)):
-            obj = self.sumRange(self.nums,nums[i][:][0],nums[i][:][1])
+            obj = self.sumRange(self.nums,nums[i][:][0])
             result.appen(obj)
 
     def sumRange(self, left: int, right: int) -> int:
