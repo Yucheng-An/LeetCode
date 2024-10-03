19b01c9bde4c43f575eb38baae007c24ff2d2b47
Yucheng-An <yuc.an2019@gmail.com>
2024-10-02 18:16:57 -0700

    Commit Python

---
+++ TreeAPI/isBalance.py
@@ -0,0 +1,20 @@
+from TreeAPI.TreeNode import TreeNode
+
+def isBalanced(root: TreeNode) -> bool:
+    """
+    Determines if the binary tree is balanced.
+    Args:
+        root (TreeNode): The root node of the tree.
+    Returns:
+        bool: True if the tree is balanced, False otherwise.
+    """
+    def checkHeight(node):
+        if node is None:
+            return 0
+        left_height = checkHeight(node.left)
+        right_height = checkHeight(node.right)
+        if left_height == -1 or right_height == -1 or abs(left_height - right_height) > 1:
+            return -1
+        return max(left_height, right_height) + 1
+
+    return checkHeight(root) != -1
