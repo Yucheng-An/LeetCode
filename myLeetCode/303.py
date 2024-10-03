3737c0bba30a53866836e85891a942fb1b87503d
Yucheng-An <yuc.an2019@gmail.com>
2024-10-03 02:09:47 -0700

    Commit Python

--- myLeetCode/303.py
+++ myLeetCode/303.py
@@ -7,7 +7,7 @@ for c in t:
         result += c
 
 for c in result:
-    need[c] = need.get(c or ord(c) - 32 == ord(c), 0) + 1
+    need[c] = need.get(c, 0) + 1
 print(need)
 
 print(ord('c')-ord('C'))
