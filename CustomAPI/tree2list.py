9ebc98737fc9a192e3062cc5ba7349ee8df1d86d
Yucheng-An <yuc.an2019@gmail.com>
2024-10-02 17:17:02 -0700

    Commit Python

--- CustomAPI/tree2list.py
+++ CustomAPI/tree2list.py
@@ -1,19 +1,5 @@
 from CustomAPI.TreeNode import TreeNode
 
-class TreeNode:
-    def __init__(self, value):
-        self.val = value
-        self.left = None
-        self.right = None
-
-    def printTree(self, level=0, prefix="Root: "):
-        if self is not None:
-            print(" " * (level * 4) + prefix + str(self.val))
-            if self.left:
-                self.left.printTree(level + 1, "L--- ")
-            if self.right:
-                self.right.printTree(level + 1, "R--- ")
-
 def tree_to_list(root: TreeNode, traversal_type: str = 'inorder') -> list[str | int]:
     """
     Converts a tree to a list based on the specified traversal type.
