ff114d9c5d0f739abd08505fbb54cedc1b9ee30c
Yucheng-An <yuc.an2019@gmail.com>
2024-10-02 17:15:39 -0700

    Commit Python

--- CustomAPI/list2Tree.py
+++ CustomAPI/list2Tree.py
@@ -1,16 +1,4 @@
-class TreeNode:
-    def __init__(self, value):
-        self.val = value
-        self.left = None
-        self.right = None
 
-    def printTree(self, level=0, prefix="Root: "):
-        if self is not None:
-            print(" " * (level * 4) + prefix + str(self.val))
-            if self.left:
-                self.left.printTree(level + 1, "L--- ")
-            if self.right:
-                self.right.printTree(level + 1, "R--- ")
 
 def list2Tree(inputList: list[str | int], root: str | int = None, balance: bool = False) -> TreeNode:
     if not inputList:
