01e2d3713746931078c9a36dd9f2f126be5befa9
Yucheng-An <yuc.an2019@gmail.com>
2024-10-02 17:21:48 -0700

    Commit Python

--- main.py
+++ main.py
@@ -3,16 +3,16 @@ import TreeAPI
 input_list = [30, 20, 40, 10, 35, 50, 5]
 input_list2 = ['d', 'b', 'f', 'a', 'e', 'g', 'c']
 
-balanceTree1 = CustomAPI.list2Tree(input_list, root=25, balance=False)
-unbalanceTree1 = CustomAPI.list2Tree(input_list, balance=True)
+balanceTree1 = TreeAPI.list2Tree(input_list, root=25, balance=False)
+unbalanceTree1 = TreeAPI.list2Tree(input_list, balance=True)
 
-balanceTree2 = CustomAPI.list2Tree(input_list2, root='c', balance=False)
-unbalanceTree2 = CustomAPI.list2Tree(input_list2, balance=True)
+balanceTree2 = TreeAPI.list2Tree(input_list2, root='c', balance=False)
+unbalanceTree2 = TreeAPI.list2Tree(input_list2, balance=True)
 
 # balanceTree1.printTree()
 # unbalanceTree1.printTree()
 #
 # balanceTree2.printTree()
 # unbalanceTree2.printTree()
-list1 = CustomAPI.tree2List(balanceTree1,"preorder")
+list1 = TreeAPI.tree2List(balanceTree1,"preorder")
 print(list1)
