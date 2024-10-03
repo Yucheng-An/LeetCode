581be35a36c6a34d6087af65c2b589cb2b626f58
Yucheng-An <yuc.an2019@gmail.com>
2024-10-02 17:06:51 -0700

    Commit Python

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ import CustomAPI
 input_list = [30, 20, 40, 10, 35, 50, 5]
 input_list2 = ['a','b','c','d','e','f']
 
-balanceTree = CustomAPI.list2Tree(input_list, root=25, balance=False)
-unbalanceTree = CustomAPI.list2Tree(input_list, balance=True)
+balanceTree1 = CustomAPI.list2Tree(input_list, root=25, balance=False)
+unbalanceTree1 = CustomAPI.list2Tree(input_list, balance=True)
 
-balanceTree = CustomAPI.list2Tree(input_list, root=25, balance=False)
-unbalanceTree = CustomAPI.list2Tree(input_list, balance=True)
+balanceTree2 = CustomAPI.list2Tree(input_list, root=25, balance=False)
+unbalanceTree2 = CustomAPI.list2Tree(input_list, balance=True)
 
-balanceTree.printTree()
-unbalanceTree.printTree()
+balanceTree1.printTree()
+unbalanceTree1.printTree()
