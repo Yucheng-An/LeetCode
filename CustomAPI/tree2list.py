673438b6a5d1fa270160b27d3b5228bcc755ea60
Yucheng-An <yuc.an2019@gmail.com>
2024-10-02 17:14:33 -0700

    Commit Python

--- CustomAPI/tree2list.py
+++ CustomAPI/tree2list.py
@@ -1,39 +1,50 @@
 from CustomAPI.list2Tree import TreeNode
 
 
-def tree_to_list(root: TreeNode, traversal_type: str = 'inorder') -> list[str | int]:
-    """
-    Converts a tree to a list based on the specified traversal type.
-    Args:
-        root (TreeNode): The root node of the tree.
-        traversal_type (str, optional): The type of traversal ('inorder', 'preorder', 'postorder').
-                                        Default is 'inorder'.
-    Returns:
-        list[str | int]: A list of values from the tree based on the chosen traversal.
-    Raises:
-        ValueError: If an invalid traversal type is provided.
-    """
-    result = []
+ef tree_to_list(root: TreeNode, traversal_type: str = 'inorder') -> list[str | int]:
+"""
+Converts a tree to a list based on the specified traversal type.
 
-    if traversal_type == 'inorder':
-        inorderTraversal(root, result)
-    elif traversal_type == 'preorder':
-        preorderTraversal(root, result)
-    elif traversal_type == 'postorder':
-        postorderTraversal(root, result)
-    else:
-        raise ValueError("Invalid traversal type. Use 'inorder', 'preorder', or 'postorder'.")
+Args:
+    root (TreeNode): The root node of the tree.
+    traversal_type (str, optional): The type of traversal ('inorder', 'preorder', 'postorder').
+                                    Default is 'inorder'.
 
-    return result
+Returns:
+    list[str | int]: A list of values from the tree based on the chosen traversal.
 
-# Helper functions for tree traversal:
-def preorderTraversal(root, result):
+Raises:
+    ValueError: If an invalid traversal type is provided.
+"""
+result = []
+
+if traversal_type == 'inorder':
+    inorderTraversal(root, result)
+elif traversal_type == 'preorder':
+    preorderTraversal(root, result)
+elif traversal_type == 'postorder':
+    postorderTraversal(root, result)
+else:
+    raise ValueError("Invalid traversal type. Use 'inorder', 'preorder', or 'postorder'.")
+
+return result
+
+# Helper function for inorder traversal:
+def inorderTraversal(root: TreeNode, result: list[str | int]):
+    if root:
+        inorderTraversal(root.left, result)
+        result.append(root.val)
+        inorderTraversal(root.right, result)
+
+# Helper function for preorder traversal:
+def preorderTraversal(root: TreeNode, result: list[str | int]):
     if root:
         result.append(root.val)
         preorderTraversal(root.left, result)
         preorderTraversal(root.right, result)
 
-def postorderTraversal(root, result):
+# Helper function for postorder traversal:
+def postorderTraversal(root: TreeNode, result: list[str | int]):
     if root:
         postorderTraversal(root.left, result)
         postorderTraversal(root.right, result)
