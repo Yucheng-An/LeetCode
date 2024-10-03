565167b3e4e24d9253b924143951cef88ad7c1c3
Yucheng-An <yuc.an2019@gmail.com>
2024-10-02 17:17:40 -0700

    Commit Python

--- main.py
+++ main.py
@@ -9,8 +9,9 @@ unbalanceTree1 = CustomAPI.list2Tree(input_list, balance=True)
 balanceTree2 = CustomAPI.list2Tree(input_list2, root='c', balance=False)
 unbalanceTree2 = CustomAPI.list2Tree(input_list2, balance=True)
 
-balanceTree1.printTree()
-unbalanceTree1.printTree()
+# balanceTree1.printTree()
+# unbalanceTree1.printTree()
+#
+# balanceTree2.printTree()
+# unbalanceTree2.printTree()
 
-balanceTree2.printTree()
-unbalanceTree2.printTree()
