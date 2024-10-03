10335887754a513923e39c4b4b36810eac01b6b8
Yucheng-An <yuc.an2019@gmail.com>
2024-10-02 17:23:26 -0700

    Commit Python

--- TreeAPI/TreeNode.py
+++ TreeAPI/TreeNode.py
@@ -5,9 +5,8 @@ class TreeNode:
         self.right = None
 
     def printTree(self, level=0, prefix="Root: "):
-        if self is not None:
-            print(" " * (level * 4) + prefix + str(self.val))
-            if self.left:
-                self.left.printTree(level + 1, "L--- ")
-            if self.right:
-                self.right.printTree(level + 1, "R--- ")
+        if self.right is not None:
+            self.right.printTree(level + 1, "    " * (level + 1) + "R--- ")
+        print("    " * level + prefix + str(self.val))
+        if self.left is not None:
+            self.left.printTree(level + 1, "    " * (level + 1) + "L--- ")
