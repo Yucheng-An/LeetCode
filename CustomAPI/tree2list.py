fb1f569f8fe68d589193ac427285b174d19d1e46
Yucheng-An <yuc.an2019@gmail.com>
2024-10-02 17:20:24 -0700

    Commit Python

--- CustomAPI/tree2list.py
+++ CustomAPI/tree2list.py
@@ -1,6 +1,6 @@
 from CustomAPI.TreeNode import TreeNode
 
-def tree_to_list(root: TreeNode, traversal_type: str = 'inorder') -> list[str | int]:
+def tree2list(root: TreeNode, traversal_type: str = 'inorder') -> list[str | int]:
     """
     Converts a tree to a list based on the specified traversal type.
     Args:
