5a8e2b6008a0e48f88827bc37f6f51e05c4e3c54
Yucheng-An <yuc.an2019@gmail.com>
2024-10-03 02:04:07 -0700

    Commit Python

--- myLeetCode/303.py
+++ myLeetCode/303.py
@@ -2,5 +2,4 @@ need, window = {}, {}
 t = "Special counsel Jack Smith provides fullest picture yet of his 2020 election case against Trump in new filing"
 for c in t:
     need[c] = need.get(c, 0) + 1
-    print(need[c])
 print(need)
