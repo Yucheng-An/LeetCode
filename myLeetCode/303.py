0f585dac39c35dbc57db7d1af8c69b037cb51321
Yucheng-An <yuc.an2019@gmail.com>
2024-10-03 02:09:27 -0700

    Commit Python

--- myLeetCode/303.py
+++ myLeetCode/303.py
@@ -1,6 +1,6 @@
 need, window = {}, {}
 t = "Special cossssssunsel Jack Smith provides fullest picture yet of his 2020 election case against Trump in new filing"
-t.lower()
+t = t.lower()
 result = ""
 for c in t:
     if c != ' ':
