0ac5ae8bfa2f9679da4eeab616c37619cac139b0
Yucheng-An <yuc.an2019@gmail.com>
2024-10-10 10:30:30 -0700

    Commit Python

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def example(num: int):
     # return re(matrix, num, 0, sizeMatrix, 1)
 
 
-def re(matrix, num, 1, ):
+def re(matrix, num, , ):
     if needCall == num:
         matrix[start][end] = 1
 
