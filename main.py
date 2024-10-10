134b85ff68a6fd3f894caf1186c24c691851b752
Yucheng-An <yuc.an2019@gmail.com>
2024-10-10 10:10:46 -0700

    Commit Python

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 def example(num: int):
     sizeMatrix = num * 2-1
     matrix = [[3]*sizeMatrix] * sizeMatrix
+    print(matrix)
     startNumber = num
     print(startNumber)
     for i in range(0,len(matrix)):
