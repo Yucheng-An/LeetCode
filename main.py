af1552a640fb74e9a46d8f21949839801963f75b
Yucheng-An <yuc.an2019@gmail.com>
2024-10-10 10:28:06 -0700

    Commit Python

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ def example(num: int):
     sizeMatrix = num * 2 - 1
     matrix = [[num] * sizeMatrix] * sizeMatrix
     print(matrix)
-    return re(matrix, num, 0, sizeMatrix, num)
+    return re(matrix, num, 0, sizeMatrix, )
 
 
 def re(matrix, num, size, needCall):
     if needCall == 1:
         matrix[start][end] = 1
-    
+
 
 
 res = example(3)
