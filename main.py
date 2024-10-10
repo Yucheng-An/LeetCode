e63abd51ceeddb8f5efe9a5b66d676562386fdb1
Yucheng-An <yuc.an2019@gmail.com>
2024-10-10 10:40:47 -0700

    Commit Python

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ def re(matrix, num, decreaseIndex):
         for i in range(0 + decreaseIndex, len(matrix) - decreaseIndex):
             for j in range(0 + decreaseIndex, len(matrix) - decreaseIndex):
                 matrix[i][j] = num - decreaseIndex
+
         return re(matrix, num, decreaseIndex + 1)
 
 
