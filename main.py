e16ce1edb14fae82ab167470d8c54087b58ddb94
Yucheng-An <yuc.an2019@gmail.com>
2024-10-10 10:15:15 -0700

    Commit Python

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 def example(num: int):
     sizeMatrix = num * 2-1
-    matrix = [[3]*sizeMatrix] * sizeMatrix
+    matrix = [[num]*sizeMatrix] * sizeMatrix
     print(matrix)
     print(num)
     for i in range(0,len(matrix)):
         for j in range(0 , len(matrix)):
-           
+
     print(matrix)
 
 
