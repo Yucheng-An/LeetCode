ca370b97d26f6942cd5187a27c3d2afbda931d2d
Yucheng-An <yuc.an2019@gmail.com>
2024-10-10 09:37:30 -0700

    Commit Python

--- main.py
+++ main.py
@@ -1,21 +1,2 @@
-# Example adjacency matrix (unweighted)
-adj_matrix = [[0, 1, 1, 0],
-              [1, 0, 1, 1],
-              [1, 1, 0, 1],
-              [0, 1, 1, 0]]
-
-# Number of nodes in the graph
-num_nodes = len(adj_matrix)
-
-# Translate the matrix to a graph (adjacency list)
-graph = {}
-
-for i in range(num_nodes):
-    graph[i] = []
-    for j in range(num_nodes):
-        if adj_matrix[i][j] != 0:  # There's an edge
-            graph[i].append(j)
-
-# Display the graph as an adjacency list
-for node, neighbors in graph.items():
-    print(f"Node {node} is connected to: {', '.join(map(str, neighbors))}")
+def example():
+    
