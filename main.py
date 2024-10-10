7fee5541603f34862f894da772e06b2a22ba4978
Yucheng-An <yuc.an2019@gmail.com>
2024-10-10 10:36:00 -0700

    Commit Python

--- main.py
+++ main.py
@@ -7,13 +7,14 @@ def example(num: int):
 
 def re(matrix, num, decreaseIndex):
     if num - decreaseIndex == 1:
-        matrix[(2*num-1)/2][(2*num-1)/2] = 1
+        matrix[(2 * num - 1) / 2][(2 * num - 1) / 2] = 1
         return matrix
 
     for i in range(0 + decreaseIndex, len(matrix) - decreaseIndex):
         for j in range(0 + decreaseIndex, len(matrix) - decreaseIndex):
             matrix[i][j] = num - decreaseIndex
-    re(matrix,num,decreaseIndex+1)
+    re(matrix, num, decreaseIndex + 1)
+
 
 res = example(3)
 print(res)
