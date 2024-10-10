d918966ec0e2d1168b86366f8035c905e50c4177
Yucheng-An <yuc.an2019@gmail.com>
2024-10-10 10:34:58 -0700

    Commit Python

--- main.py
+++ main.py
@@ -13,6 +13,6 @@ def re(matrix, num, decreaseIndex ):
     for i in range(0 + decreaseIndex, len(matrix) - decreaseIndex):
         for j in range(0 + decreaseIndex, len(matrix) - decreaseIndex):
             matrix[i][j] = num - decreaseIndex
-    re
+    re(matrix,num,decreaseIndex+1)
 res = example(3)
 print(res)
