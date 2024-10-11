54a1b7f64c4a32ce4d1360436f49dceb9526c751
Yucheng-An <yuc.an2019@gmail.com>
2024-10-11 13:49:22 -0700

    Commit Python

--- myLeetCode/1942.py
+++ myLeetCode/1942.py
@@ -1,10 +1,6 @@
 times = [[3,10],[1,5],[2,6]]
-targetFriend = 1
-
-
 friendList = []
 for i in range(len(times)):
     friendList.append((times[i],i))
-
 print(friendList)
 friendList.sort(key=lambda x: x[0][0])
