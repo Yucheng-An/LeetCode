d2b5f77d3c00c6a3814302488d33e5c812f6412e
Yucheng-An <yuc.an2019@gmail.com>
2024-10-12 16:59:43 -0700

    Commit Python

--- myLeetCode/1942.py
+++ myLeetCode/1942.py
@@ -1,11 +1,1 @@
-times = [[3,10],[1,5],[2,6]]
-friendList = []
-for i in range(len(times)):
-    friendList.append((times[i],i))
-
-friendList.sort(key=lambda x: x[0][0])
-
-chairList = []
-print(friendList)
-chairList.append(friendList[0])
-for i in range(1,len(friendList)):
+dicransom = {}
