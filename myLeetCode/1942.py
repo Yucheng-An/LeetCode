3c9a7cb1595843cf256cb65dd2305f4b429d9ee8
Yucheng-An <yuc.an2019@gmail.com>
2024-10-12 17:04:28 -0700

    Commit Python

--- myLeetCode/1942.py
+++ myLeetCode/1942.py
@@ -3,7 +3,7 @@ magazine = "aab"
 dicransom = {}
 for i in ransomNote:
     if i in dicransom:
-        dicransom[i] += 1
+        dicransom[i]  1
     if i not in dicransom:
         dicransom:{i:0}
 print(dicransom)
