d04ee161809bf1de8617385d9dcea0c30746b361
Yucheng-An <yuc.an2019@gmail.com>
2024-10-12 17:05:40 -0700

    Commit Python

--- myLeetCode/1942.py
+++ myLeetCode/1942.py
@@ -1,8 +1,8 @@
 ransomNote = "aaaaaa"
 magazine = "aab"
-dicransom = {}
+dicRansomNote = {}
 for i in ransomNote:
-    if i in dicransom:
-        dicransom[i] += 1
-    if i not in dicransom:
-        dicransom[i] = 1
+    if i in dicRansomNote:
+        dicRansomNote[i] += 1
+    if i not in dicRansomNote:
+        dicRansomNote[i] = 1
