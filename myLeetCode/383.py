38023b1a0b915df21c6c3a404827f8bc9fe0c70d
Yucheng-An <yuc.an2019@gmail.com>
2024-10-12 17:09:10 -0700

    Commit Python

--- myLeetCode/383.py
+++ myLeetCode/383.py
@@ -1,17 +1,3 @@
-ransomNote = "aa"
-magazine = "aab"
-dicRansomNote = {}
-for i in magazine:
-    if i in dicMagazine:
-        dicMagazine[i] += 1
-    if i not in dicMagazine:
-        dicMagazine[i] = 1
-print(dicMagazine)
-for i in ransomNote:
-    if i in dicMagazine:
-        dicMagazine[i] -= 1
-        if dicMagazine[i] == -1:
-            return False
-    else:
-        return False
-return True
+def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+    dicransom = {}
+    for i in ransomNode:
