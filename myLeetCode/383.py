889df3b509bc7f07591a5ca7b2f3eb8a6c7763d4
Yucheng-An <yuc.an2019@gmail.com>
2024-10-12 17:09:44 -0700

    Commit Python

--- myLeetCode/383.py
+++ myLeetCode/383.py
@@ -1,8 +1,8 @@
 def canConstruct(self, ransomNote: str, magazine: str) -> bool:
     dicRansomNote = {}
         for i in magazine:
-    if i in dicMagazine:
-        dicMagazine[i] += 1
+            if i in dicMagazine:
+                dicMagazine[i] += 1
     if i not in dicMagazine:
         dicMagazine[i] = 1
 print(dicMagazine)
