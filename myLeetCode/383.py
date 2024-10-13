fd871324e101fd883ef55a1a1f6fa5fc95403910
Yucheng-An <yuc.an2019@gmail.com>
2024-10-12 17:09:02 -0700

    Commit Python

---
+++ myLeetCode/383.py
@@ -0,0 +1,17 @@
+ransomNote = "aa"
+magazine = "aab"
+dicRansomNote = {}
+for i in magazine:
+    if i in dicMagazine:
+        dicMagazine[i] += 1
+    if i not in dicMagazine:
+        dicMagazine[i] = 1
+print(dicMagazine)
+for i in ransomNote:
+    if i in dicMagazine:
+        dicMagazine[i] -= 1
+        if dicMagazine[i] == -1:
+            return False
+    else:
+        return False
+return True
