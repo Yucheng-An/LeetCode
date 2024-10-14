ad1c03efb5adc9a966714476d29f5c8cba49d3e5
Yucheng-An <yuc.an2019@gmail.com>
2024-10-14 16:30:58 -0700

    Commit Python

--- myLeetCode/632.py
+++ myLeetCode/632.py
@@ -21,7 +21,7 @@ def find_missing(arr, left, right, start):
         return find_missing(arr, left, mid, start)
 
 # Example usage:
-arr = [9, 10, 11, 12, 13, 1, 8]
+arr = [9, 10, 11, 12, 13, 15, 16]
 missing_number = find_missing(arr, 0, len(arr), arr[0])
 print("The missing number is:", missing_number)
 
