0e7dc97daa69a629b6562dbeecd5def5927756be
Yucheng-An <yuc.an2019@gmail.com>
2024-10-14 16:18:08 -0700

    Commit Python

--- myLeetCode/632.py
+++ myLeetCode/632.py
@@ -1,10 +1,12 @@
-def find (il):
+def find(il):
     if len(il) == 1:
         return None
-    if il[0]-il[1]==-1:
+    if il[0] - il[1] == -1:
         il.pop()
         find(il)
     else:
-        return il[1]-1
+        return il[1] - 1
 
-t = [0,1,3,4,5,]
+
+t = [0, 1, 3, 4, 5, 6]
+print(find(t))
