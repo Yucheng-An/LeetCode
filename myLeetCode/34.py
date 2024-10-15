f944f05e89721072ba62e6965812c1db612ca7d2
Yucheng-An <yuc.an2019@gmail.com>
2024-10-15 10:54:22 -0700

    Commit Python

---
+++ myLeetCode/34.py
@@ -0,0 +1,30 @@
+def searchRange(self, nums: List[int], target: int) -> List[int]:
+    return [left_bound(nums,target),right_bound(nums,target)]
+
+def left_bound(nums: List[int], target: int) -> int:
+    left, right = 0, len(nums) - 1
+    while left <= right:
+        mid = left + (right - left) // 2
+        if nums[mid] < target:
+            left = mid + 1
+        elif nums[mid] > target:
+            right = mid - 1
+        elif nums[mid] == target:
+            right = mid - 1
+    if left < 0 or left >= len(nums):
+        return -1
+    return left if nums[left] == target else -1
+
+def right_bound(nums: List[int], target: int) -> int:
+    left, right = 0, len(nums) - 1
+    while left <= right:
+        mid = left + (right - left) // 2
+        if nums[mid] < target:
+            left = mid + 1
+        elif nums[mid] > target:
+            right = mid - 1
+        elif nums[mid] == target:
+            left = mid + 1
+    if right < 0 or right >= len(nums):
+        return -1
+    return right if nums[right] == target else -1
