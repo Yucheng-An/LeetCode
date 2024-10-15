a067d0ac9b5033f68f447c63b4094e5ab1420879
Yucheng-An <yuc.an2019@gmail.com>
2024-10-15 10:57:54 -0700

    Commit Python

--- myLeetCode/34.py
+++ myLeetCode/34.py
@@ -15,8 +15,8 @@ def left_bound(nums, target):
         return -1
     return left if nums[left] == target else -1
 def right_bound(nums, target):
-    left, right = 0, len(nums) - 1
-    while left <= right:
+    left, right = 0, len(nums)
+    while left < right:
         mid = left + (right - left) // 2
         if nums[mid] < target:
             left = mid + 1
