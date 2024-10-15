2b700035ec9ccff532976bae32761c92e01f44eb
Yucheng-An <yuc.an2019@gmail.com>
2024-10-14 20:55:24 -0700

    Commit Python

---
+++ myLeetCode/704.py
@@ -0,0 +1,13 @@
+def search(nums: List[int], target: int) -> int:
+    return binarySearch(nums,target,0,len(nums)-1)
+
+def binarySearch(nums,target,left,right):
+    while left < right:
+        mid = left + ((right - left)/2)
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] > target:
+            right = mid - 1
+        elif nums[mid] < target:
+            left = mid + 1
+    return -1
