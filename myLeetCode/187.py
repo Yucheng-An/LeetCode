ffa609f0090fd10d4647cc5546a56b2b2d06f15f
Yucheng-An <yuc.an2019@gmail.com>
2024-10-14 20:42:30 -0700

    Commit Python

---
+++ myLeetCode/187.py
@@ -0,0 +1,26 @@
+def findRepeatedDnaSequences(self, s: str) -> List[str]:
+    nums =[0] * len(s)
+    for i in s:
+        if s == 'A':
+            nums[i] = 1
+        elif s == 'C':
+            nums[i] = 2
+        elif s == 'G':
+            nums[i] = 3
+        elif s == 'T':
+            nums[i] = 4
+    hashNumber = 0
+    seen = set()
+    res = set()
+    left , right = 0,0
+    while right < len(nums):
+        hashNumber = hashNumber * 10 + nums[right]
+        right += 1
+        if right - left == 10:
+            if hashNumber in seen:
+                res.add(s[left:right])
+            else:
+                seen.add(hashNumber)
+            hashNumber = hashNumber - (nums[left] * (10**9))
+            left += 1
+    return list(res)
