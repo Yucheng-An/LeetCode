0850147ee1f18f6ff90ca7a76899dd0825f19fd4
Yucheng-An <yuc.an2019@gmail.com>
2024-10-14 20:58:36 -0700

    Commit Python

--- myLeetCode/704.py
+++ myLeetCode/704.py
@@ -1,7 +1,8 @@
 def search(nums, target):
     left, right = 0, len(nums)
     while left < right:
-        mid = left + 
+        mid = left + ((right - left)//2)
+        if nums[mid] = 
 
 nums = [-1,0,2,3,5,9,12]
 target = 2
