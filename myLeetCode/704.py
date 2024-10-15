1938b9c2357ad8874b7bfcce17e70d7678432488
Yucheng-An <yuc.an2019@gmail.com>
2024-10-14 20:57:46 -0700

    Commit Python

--- myLeetCode/704.py
+++ myLeetCode/704.py
@@ -1,16 +1,5 @@
 def search(nums, target):
-    return binarySearch(nums,target,0,len(nums)-1)
-
-def binarySearch(nums,target,left,right):
-    while left < right:
-        mid = left + ((right - left)//2)
-        if nums[mid] == target:
-            return mid
-        elif nums[mid] > target:
-            right = mid - 1
-        elif nums[mid] < target:
-            left = mid + 1
-    return -1
+    
 
 nums = [-1,0,2,3,5,9,12]
 target = 2
