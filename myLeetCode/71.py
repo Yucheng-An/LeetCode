4f593ec29151aa7bfb9f8abdc97085d12b60d6bc
Yucheng-An <yuc.an2019@gmail.com>
2024-10-20 15:27:31 -0700

    Commit Python

---
+++ myLeetCode/71.py
@@ -0,0 +1,15 @@
+def simplifyPath(self, path: str) -> str:
+    parts = path.split("/")
+    stk = []
+    for part in parts:
+        if part == "" or part == ".":
+            continue
+        if part == "..":
+            if stk:
+                stk.pop()
+            continue
+        stk.append(part)
+    res = ""
+    while stk:
+        res = "/" + stk.pop() + res
+    return res if res else "/"
