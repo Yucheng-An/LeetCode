bbfced41415f2fdb07ba277da659b932ee8bfc2d
Yucheng-An <yuc.an2019@gmail.com>
2024-10-20 14:34:14 -0700

    Commit Python

--- myLeetCode/1106.py
+++ myLeetCode/1106.py
@@ -14,7 +14,7 @@ def parseBoolExpr(expression):
     for i in boolValue:
         
 def subExp(subExpr):
-    boolValue = []
+    boolValue = set()
     for i in range(2,len(subExpr)-1):
         if subExpr[i] == 't': 
             boolValue.append('t')
