4d72c2533e791f6160202d5258787d8218b5a3d5
Yucheng-An <yuc.an2019@gmail.com>
2024-10-20 14:42:29 -0700

    Commit Python

--- myLeetCode/1106.py
+++ myLeetCode/1106.py
@@ -4,14 +4,8 @@ testStr = "|(f,f,f,t)"
 
 
 def parseBoolExpr(expression):
-    calculation = []
-    boolValue = []
     for i in expression:
-        if i == 'f' or i == 't':
-            boolValue.append(i)
-        elif i == '|' or i == '&':
-            calculation.append(i)
-
+        
 
 def subExp(subExpr):
     global mixed, allTrue, allFalse
