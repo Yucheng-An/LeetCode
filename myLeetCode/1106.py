f409d1c83d1646ab00d718b251df87903800f7d4
Yucheng-An <yuc.an2019@gmail.com>
2024-10-20 14:35:51 -0700

    Commit Python

--- myLeetCode/1106.py
+++ myLeetCode/1106.py
@@ -24,7 +24,8 @@ def subExp(subExpr):
     allFalse = None
     mixed= None
     
-    if len(boolValue) ==  2:
+    if 
+    elif len(boolValue) ==  2:
         mixed = True
     else: 
         for len()
