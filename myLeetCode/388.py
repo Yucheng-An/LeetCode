4446c6f6287ad15204c487640c51cc6e549c05eb
Yucheng-An <yuc.an2019@gmail.com>
2024-10-20 22:12:58 -0700

    Commit Python

--- myLeetCode/388.py
+++ myLeetCode/388.py
@@ -13,4 +13,4 @@ def lengthLongestPath( input):
     return max_len
 
 input = "dir\n\tsubdir1\n\t\tfile1.ext\n\t\tsubsubdir1\n\tsubdir2\n\t\tsubsubdir2\n\t\t\tfile2.ext"
-print()
+print(lengthLongestPath(input))
