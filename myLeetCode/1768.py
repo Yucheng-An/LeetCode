3e62e3b8f0781d06f1295e3862e2c85ebf9c4a42
Yucheng-An <yuc.an2019@gmail.com>
2024-10-21 00:33:00 -0700

    Commit Python

--- myLeetCode/1768.py
+++ myLeetCode/1768.py
@@ -8,8 +8,8 @@ def mergeAlternately(word1, word2):
         p1 += 1
         p2 += 1
     if p1 < len(word1):
-        for i in range(p2,len(word2)):
-            res += word2[p2]
+        for i in range(p1,len(word1)):
+            res += word1[p1]
     else:
         for i in range(p2,len(word2)):
             res += word2[p2]
