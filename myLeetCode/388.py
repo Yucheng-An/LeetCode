5f82e0675f237414e7d52d03a19e9e683bd142b9
Yucheng-An <yuc.an2019@gmail.com>
2024-10-20 22:24:09 -0700

    Commit Python

--- myLeetCode/388.py
+++ myLeetCode/388.py
@@ -17,6 +17,6 @@ def lengthLongestPath(input):
 input = "dir\n\tsubdir1\n\t\tfile1.ext\n\t\tsubsubdir1\n\tsubdir2\n\t\tsubsubdir2\n\t\t\tfile2.ext"
 print(lengthLongestPath(input))
 
-s = "hello"
-index = s.rfind("he")
+s = "hhhello"
+index = s.rfind("h")
 print(index)
