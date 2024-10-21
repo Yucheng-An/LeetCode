f8e1d97facc2caf1b468d6333849361e3287309f
Yucheng-An <yuc.an2019@gmail.com>
2024-10-21 15:04:32 -0700

    Commit Python

--- myLeetCode/28.py
+++ myLeetCode/28.py
@@ -1,19 +1,22 @@
-def strStr(haystack: str, needle: str) -> int:
-    if len(haystack) < len(needle):
-        return -1
-    h = 0
-    while h < len(haystack):
-        t = h
-        for n in range(len(needle)):
-            if needle[n] == haystack[t]:
-                if n == len(needle) - 1:
-                    return h
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        if len(haystack) < len(needle):
+            return -1
+        h = 0
+        while h < len(haystack):
+            t = h
+            for n in range(len(needle)):
+                if needle[n] == haystack[t]:
+                    if n == len(needle) - 1:
+                        return h
+                    elif t + 1 < len(haystack):
+                        t += 1
+                    else:
+                        return -1
                 else:
-                    t += 1
-            else:
-                break
-        h+=1
-    return -1
+                    break
+            h+=1
+        return -1
 
 haystack ="mississippi"
 needle ="issipi"
