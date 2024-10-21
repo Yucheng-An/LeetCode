b582afbe7cb730eee7ece3dddc89ce023b8ff595
Yucheng-An <yuc.an2019@gmail.com>
2024-10-21 14:11:49 -0700

    Commit Python

--- myLeetCode/22.py
+++ myLeetCode/22.py
@@ -1,7 +1,7 @@
 def generateParenthesis(n):
     # dp[i] will store all the combinations of well-formed parentheses with i pairs
     dp = [[] for _ in range(n + 1)]
-
+    res = []
     # Base case: there's only one combination with 0 pairs of parentheses
     dp[0] = [""]
 
