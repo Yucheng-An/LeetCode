abec5e94c4c0fdf480cea8c99da9096c4c4e4908
Yucheng-An <yuc.an2019@gmail.com>
2024-10-20 22:12:15 -0700

    Commit Python

--- myLeetCode/388.py
+++ myLeetCode/388.py
@@ -1,10 +1,8 @@
 def lengthLongestPath( input):
-    # 这个栈存储之前的父路径。实际上这里只用存父路径的长度就够了，这个优化留给你吧
     stack = []
     max_len = 0
     for part in input.split("\n"):
         level = part.rfind("\t") + 1
-        # 让栈中只保留当前目录的父路径
         while level < len(stack):
             stack.pop()
         stack.append(len(part) - level)
