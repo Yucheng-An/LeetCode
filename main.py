c1c484bb87f75cc2f9f610db52d7d78bcc518eb6
Yucheng-An <yuc.an2019@gmail.com>
2024-10-23 20:26:26 -0700

    Commit Python

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 def maxQualitySegmentation(y):
     n = len(y)
     OPT = [0] * (n + 1)
-
-    # Iterate over all characters in y to fill OPT
     for i in range(1, n + 1):
-        # Find the best segmentation ending at position i
         max_quality = float('-inf')
         for j in range(1, i + 1):
             # Consider the word y[j-1:i] (from j to i inclusive)
