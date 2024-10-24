3b817f89ce8ef49558bf8501ed4f8af85cb1c618
Yucheng-An <yuc.an2019@gmail.com>
2024-10-23 20:30:50 -0700

    Commit Python

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ def maxQualityOfString(y):
     n = len(y)
     OPT = [0] * (n + 1)
     for i in range(1, n + 1):
-        max_quality = float('-inf')
+        maxQuality = float('-inf')
         for j in range(1, i + 1):
             newWord = y[j-1:i]
             current_quality = quality(newWord) + OPT[j - 1]
-            max_quality = max(max_quality, current_quality)
-        OPT[i] = max_quality
+            maxQuality = max(maxQuality, current_quality)
+        OPT[i] = maxQuality
     return OPT[n]
