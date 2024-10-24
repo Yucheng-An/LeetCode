0eadc278611a468e0c184ddbe489ce1dfc69977b
Yucheng-An <yuc.an2019@gmail.com>
2024-10-23 19:30:34 -0700

    Commit Python

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-def minCostSchedule(weekNumber, supplyList, companyARate, c):
+def minCostSchedule(weekNumber, supplyList, companyA_Rate, c):
     OPT = [float('inf')] * (weekNumber + 1)
     OPT[0] = 0
     if weekNumber >= 1:
-        OPT[1] = companyARate * supplyList[0]
+        OPT[1] = companyA_Rate * supplyList[0]
     if weekNumber >= 2:
-        OPT[2] = companyARate * supplyList[0] + companyARate * supplyList[1]
+        OPT[2] = companyA_Rate * supplyList[0] + companyA_Rate * supplyList[1]
     if weekNumber >= 3:
-        OPT[3] = companyARate * supplyList[0] + companyARate * supplyList[1] + companyARate * supplyList[2]
+        OPT[3] = companyA_Rate * supplyList[0] + companyA_Rate * supplyList[1] + companyA_Rate * supplyList[2]
     for i in range(4, weekNumber + 1):
-        optionA = companyARate * supplyList[i - 1] + OPT[i - 1]
+        optionA = companyA_Rate * supplyList[i - 1] + OPT[i - 1]
         optionB = float('inf')
         if 4 <= i <= weekNumber-4:
             optionB = c * 4 + OPT[i - 4]
