f432dade4077fafeaeee865c096c342eba62e946
Yucheng-An <yuc.an2019@gmail.com>
2024-10-23 19:39:09 -0700

    Commit Python

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 def minCostSchedule(weekNumber, supplyList, companyA_Rate, companyB_Amount):
     OPT = [float('inf')] * (weekNumber + 1)
     OPT[0] = 0
-    for i in range(1,4):
-        OPT[i] = OPT[i-1] + companyA_Rate * supplyList[i-1]
+    for i in range(1, 4):
+        OPT[i] = OPT[i - 1] + companyA_Rate * supplyList[i - 1]
     for i in range(4, weekNumber + 1):
         chooseA = companyA_Rate * supplyList[i - 1] + OPT[i - 1]
         chooseB = float('inf')
@@ -11,9 +11,10 @@ def minCostSchedule(weekNumber, supplyList, companyA_Rate, companyB_Amount):
         OPT[i] = min(chooseA, chooseB)
     return OPT[weekNumber]
 
+
 # Example Usage:
 n = 10
-s = [11, 9, 9, 12, 12, 12, 12, 9, 9, 11]   # Weekly supply in pounds
+s = [11, 9, 9, 12, 12, 12, 12, 9, 9, 11]  # Weekly supply in pounds
 r = 1  # Cost per pound for Company A
 c = 10  # Fixed weekly cost for Company B
 
