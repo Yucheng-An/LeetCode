7d686adc4380ff46172b761e26733455e05bab73
Yucheng-An <yuc.an2019@gmail.com>
2024-11-14 09:15:06 -0800

    Commit Python

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ def letter_combinations(digits):
                 ['p', 'q', 'r', 's'], ['t', 'u', 'v'], ['w', 'x', 'y', 'z']]
 
     def backtrack(index, path):
+        keyboard = [[], [], ['a', 'b', 'c'], ['d', 'e', 'f'], ['g', 'h', 'i'], ['j', 'k', 'l'], ['m', 'n', 'o'],
+                    ['p', 'q', 'r', 's'], ['t', 'u', 'v'], ['w', 'x', 'y', 'z']]
         if index == len(digits):
             combinations.append("".join(path))
             return
