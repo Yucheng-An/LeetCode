ebd5b6abb05bc1fcc711ebcc283bdae94662d70d
Yucheng-An <yuc.an2019@gmail.com>
2024-11-14 08:56:53 -0800

    Commit Python

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 def letterCombinations(digits):
     # 23
-    keyboard = [[],[],['a','b','c'],['d','e','f'],['g','h','i'],['j','k','l'],['m','n','o'],['p','q','r','s'],['t','u','v'],['w','x','y','z']]
+    keyboard = [[], [], ['a', 'b', 'c'], ['d', 'e', 'f'], ['g', 'h', 'i'], ['j', 'k', 'l'], ['m', 'n', 'o'],
+                ['p', 'q', 'r', 's'], ['t', 'u', 'v'], ['w', 'x', 'y', 'z']]
     allCombine = []
     for i in range(len(digits)):
         for j in keyboard[int(digits[i])]:
