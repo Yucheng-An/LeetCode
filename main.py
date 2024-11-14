c210fa09244ffc57a93aaacabfa9f67eac2dc2cd
Yucheng-An <yuc.an2019@gmail.com>
2024-11-14 09:12:20 -0800

    Commit Python

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
-def letterCombinations(digits):
-    # 23
-    keyboard = [[], [], ['a', 'b', 'c'], ['d', 'e', 'f'], ['g', 'h', 'i'], ['j', 'k', 'l'], ['m', 'n', 'o'],
-                ['p', 'q', 'r', 's'], ['t', 'u', 'v'], ['w', 'x', 'y', 'z']]
-    subKeyboard = []
-    allCombine = []
-    for i in range(len(digits)):
-        subKeyboard.append(keyboard[int(digits[i])])
-    print(subKeyboard)
-    n = len(subKeyboard)
-    
+def letter_combinations(digits):
+    if not digits:
+        return []
 
-letterCombinations("234")
+    # Mapping of digits to corresponding letters
+    digit_to_char = {
+        "2": "abc", "3": "def", "4": "ghi", "5": "jkl",
+        "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"
+    }
+
+    # Backtracking function to generate combinations
+    def backtrack(index, path):
+        if index == len(digits):
+            combinations.append("".join(path))
+            return
+        possible_chars = digit_to_char[digits[index]]
+        for char in possible_chars:
+            path.append(char)
+            backtrack(index + 1, path)
+            path.pop()  # backtrack
+
+    combinations = []
+    backtrack(0, [])
+    return combinations
+
+# Example usage
+digits = "23"
+print(letter_combinations(digits))
