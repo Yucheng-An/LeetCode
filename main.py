0ca298b987882fbbf5579d0c608740e1365161f5
Yucheng-An <yuc.an2019@gmail.com>
2024-11-14 08:54:21 -0800

    Commit Python

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 def letterCombinations(digits):
     keyboard = [[],[],['a','b','c'],['d','e','f'],['g','h','i'],['j','k','l'],['m','n','o'],['p','q','r','s'],['t','u','v'],['w','x','y','z']]
         for i in range(len(digits)):
-            for keyboard[int()]
+            for keyboard[int(digits[i])]
