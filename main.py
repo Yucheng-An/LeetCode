9ae60a5de3e8d26a0d9bcab89072af28cacf04bc
Yucheng-An <yuc.an2019@gmail.com>
2024-11-14 08:55:25 -0800

    Commit Python

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 def letterCombinations(digits):
+    # 23
     keyboard = [[],[],['a','b','c'],['d','e','f'],['g','h','i'],['j','k','l'],['m','n','o'],['p','q','r','s'],['t','u','v'],['w','x','y','z']]
     allCombine = []
     for i in range(len(digits)):
