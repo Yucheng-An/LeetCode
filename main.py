03255120ce3926db4073670254b777b1101fae76
Yucheng-An <yuc.an2019@gmail.com>
2024-11-25 15:21:48 -0800

    Commit Python

--- main.py
+++ main.py
@@ -2,4 +2,11 @@ A= [1,2,3,4,5,6,7,8]
 x = 6
 
 def func (A,x):
-    
+    if len(A) == 0:
+        return None
+    if len(A) ==1 :
+        if A[0]>x:
+            return 1
+        else:
+            return 0
+    left = A[]
