c2b33bc66d90411918124c56e4829d92ec09e184
Yucheng-An <yuc.an2019@gmail.com>
2024-11-30 19:01:45 -0800

    Commit Python

--- main.py
+++ main.py
@@ -12,14 +12,8 @@ def largest(min_factor, max_factor):
 
 
 def smallest(min_factor, max_factor):
-    """Given a range of numbers, find the smallest palindromes which
-    are products of two numbers within that range.
-
-    :param min_factor: int with a default value of 0
-    :param max_factor: int
-    :return: tuple of (palindrome, iterable).
-    Iterable should contain both factors of the palindrome in an arbitrary order.
-    """
-
 
     pass
+
+
+value, factors = smallest(min_factor=1, max_factor=9)
