5ded4b3fbbc221016e82c62e7180d416dc3a5398
Yucheng-An <yuc.an2019@gmail.com>
2024-11-30 18:55:25 -0800

    Commit Python

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def func(A, x):
     if len(A) == 0:
         return None
     if len(A) == 1:
-        return 1 if 
+        return 1 if A[0] > x else 
     left = A[:len(A) // 2]
     right = A[len(A) // 2:]
     return right + left
