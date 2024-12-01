d6d1e8a9ed85710fe0f60fe3be014105bdc5c881
Yucheng-An <yuc.an2019@gmail.com>
2024-11-30 19:18:37 -0800

    Commit Python

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ def checkPalindrome(s):
     for i in range(0,len(s)//2+1):
         stack.append(s[i])
     print(stack)
-    for i in range()
+    for i in range(len(s)//2+1)
         
 checkPalindrome("1")
 smallest(1, 9)
