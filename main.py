0a93f7b891be0092f0265c41876adcea3c6b755d
Yucheng-An <yuc.an2019@gmail.com>
2024-11-30 18:55:57 -0800

    Commit Python

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 A = [1, 2, 3, 4, 5, 6, 7, 8]
 x = 6
 
-
-def func(A, x):
+def count_greater(A, x):
+    # Base case: If the array is empty, return 0
     if len(A) == 0:
         return 0
+    # Base case: If the array has one element, check if it is greater than x
     if len(A) == 1:
         return 1 if A[0] > x else 0
-    left = A[:len(A) // 2]
-    right = A[len(A) // 2:]
-    return right + left
+    # Divide the array into two halves
+    mid = len(A) // 2
+    left = A[:mid]
+    right = A[mid:]
+    # Recursively count in each half
+    count_left = count_greater(left, x)
+    count_right = count_greater(right, x)
+    # Combine the results
+    return count_left + count_right
 
-
-res = func(A, x)
+res = count_greater(A, x)
 print(res)
