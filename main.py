e0b758e3588a139bd2fc546965d7821177aa163c
Yucheng-An <yuc.an2019@gmail.com>
2024-11-30 18:58:03 -0800

    Commit Python

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-def count_greater(inputArray, target):
+def count_greater(inputArray, x):
     if len(inputArray) == 0:
         return 0
     if len(inputArray) == 1:
-        return 1 if inputArray[0] > target else 0
+        return 1 if inputArray[0] > x else 0
     left = inputArray[:len(inputArray) // 2]
     right = inputArray[len(inputArray) // 2:]
-    return count_greater(left, target) + count_greater(right, target)
+    return count_greater(left, x) + count_greater(right, x)
 
 
 A = [1, 2, 3, 4, 5, 6, 7, 8]
