6328e87a8afdffa3b2c8b2a6a4fd681336af2397
Yucheng-An <yuc.an2019@gmail.com>
2024-11-30 18:58:33 -0800

    Commit Python

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ def count_greater(inputArray, targetVlue):
     if len(inputArray) == 1:
         return 1 if inputArray[0] > targetVlue else 0
     right = inputArray[len(inputArray) // 2:]
-    return count_greater(inputArray[:len(inputArray) // 2], targetVlue) + count_greater(right, targetVlue)
+    return count_greater(inputArray[:len(inputArray) // 2], targetVlue) + count_greater(inputArray[len(inputArray) // 2:], targetVlue)
 
 
 A = [1, 2, 3, 4, 5, 6, 7, 8]
