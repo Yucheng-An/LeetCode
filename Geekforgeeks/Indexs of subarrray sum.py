bec6a030d0d5062da0107fc35a05e1d8f44fe888
Yucheng-An <yuc.an2019@gmail.com>
2024-12-03 23:09:26 -0800

    Commit Python

---
+++ Geekforgeeks/Indexs of subarrray sum.py
@@ -0,0 +1,17 @@
+def subArraySum(arr, target):
+    sum = 0
+    res = []
+    left = 0
+    while left < len(arr):
+        for i in range(left,len(arr)):
+            sum += arr[i]
+            res.append(i)
+            if sum > target:
+                left += 1
+                sum = 0
+                res = []
+                break
+            if sum == target:
+                return res
+    return [-1]
+
