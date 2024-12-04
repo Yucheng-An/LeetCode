4e1a1a50131c1d9c31463d9f2a8659c9f27f6dcd
Yucheng-An <yuc.an2019@gmail.com>
2024-12-03 23:46:20 -0800

    Commit Python

--- Geekforgeeks/Indexs of subarrray sum.py
+++ Geekforgeeks/Indexs of subarrray sum.py
@@ -1,17 +1,19 @@
 def subArraySum(arr, target):
-    sum,left,res = 0,0,[]
+    sum, left, res = 0, 0, []
     while left < len(arr):
-        res.append(left+1)
-        for i in range(left,len(arr)):
+        res.append(left + 1)
+        for i in range(left, len(arr)):
             sum = sum + arr[i]
             if sum > target:
                 left += 1
                 sum = 0
                 res.clear()
             if sum == target:
-                res.append(i+1)
+                res.append(i + 1)
                 return res
     return [-1]
-testcase = [1,2,3,7,5]
+
+
+testcase = [1, 2, 3, 7, 5]
 target = 12
-print(subArraySum(testcase,target))
+print(subArraySum(testcase, target))
