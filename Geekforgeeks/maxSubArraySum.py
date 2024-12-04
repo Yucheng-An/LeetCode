ce06cf47906d5b1224878da7dbd1c67b4423ba46
Yucheng-An <yuc.an2019@gmail.com>
2024-12-04 00:36:43 -0800

    Commit Python

---
+++ Geekforgeeks/maxSubArraySum.py
@@ -0,0 +1,18 @@
+def maxSubArraySum(arr):
+    left = 0
+    maxResult = float("-inf")
+    currentSum = 0
+    for i in range(len(arr)):
+        currentSum += arr[i]
+        if currentSum > maxResult:
+            maxResult = currentSum
+        subCurrentSum = currentSum
+        while left <= i:
+            subCurrentSum -= arr[left]
+            if subCurrentSum > maxResult:
+                maxResult = subCurrentSum
+            left += 1
+    return maxResult
+
+
+
