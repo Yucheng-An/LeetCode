58e067e5400ed477f83098d1771263691c560c1e
Yucheng-An <yuc.an2019@gmail.com>
2024-12-04 00:01:08 -0800

    Commit Python

--- Geekforgeeks/Indexs of subarrray sum.py
+++ Geekforgeeks/Indexs of subarrray sum.py
@@ -23,8 +23,8 @@ def subArraySum2(arr, target):
    start = 0
    currSum = 0
    for i in arr:
-       currSum += 
-
+       currSum += i
+    
 testcase = [22,9,47,33,32]
 target = 145
 print(subArraySum(testcase, target))
