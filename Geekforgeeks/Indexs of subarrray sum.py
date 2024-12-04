cefee6c18408a9a7ba3893980d84b40b0ddf6560
Yucheng-An <yuc.an2019@gmail.com>
2024-12-03 23:45:50 -0800

    Commit Python

--- Geekforgeeks/Indexs of subarrray sum.py
+++ Geekforgeeks/Indexs of subarrray sum.py
@@ -13,7 +13,7 @@ def subArraySum(arr, target):
                 return res
     return [-1]
 testcase = [1,2,3]
-testcase.clear()
+
 print(testcase)
 target = 12 
 # print(subArraySum(testcase,target))
