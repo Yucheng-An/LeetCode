ecf235b25a6d9b5dd82887cc34249a159d272036
Yucheng-An <yuc.an2019@gmail.com>
2024-12-05 11:30:20 -0800

    Commit Python

--- Geekforgeeks/leader.py
+++ Geekforgeeks/leader.py
@@ -1,18 +1,11 @@
-def leaders(arr):
-    n = len(arr)
-    leaders = []
-    max_from_right = float('-inf')  # Initialize with the smallest possible value
-
-    # Traverse the array from right to left
-    for i in range(n - 1, -1, -1):
-        if arr[i] >= max_from_right:
-            leaders.append(arr[i])
-            max_from_right = arr[i]
-
-    # Reverse the leaders to get the correct order
-    leaders.reverse()
-    return leaders
-
-# Example usage
+def leaders(self, arr):
+    res = []
+    currentMax = float("-inf")
+    for i in range(len(arr)-1, -1, -1):
+        if arr[i] >= currentMax:
+            res.append(arr[i])
+            currentMax = arr[i]
+    res.reverse()
+    return res
 arr = [16, 17, 4, 3, 5, 2]
 print("Leaders:", leaders(arr))
