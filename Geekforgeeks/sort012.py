29e6dd86e67b55a12939f4f34dd77458047477bb
Yucheng-An <yuc.an2019@gmail.com>
2024-12-05 11:33:34 -0800

    Commit Python

---
+++ Geekforgeeks/sort012.py
@@ -0,0 +1,12 @@
+def sort012(self, arr):
+    zeroList = []
+    oneList = []
+    twoList = []
+    for i in arr:
+        if i==0:
+            zeroList.append(i)
+        elif i == 1:
+            oneList.append(i)
+        elif i == 2:
+            twoList.append(i)
+    return zeroList + oneList + twoList
