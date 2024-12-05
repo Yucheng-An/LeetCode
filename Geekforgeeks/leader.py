cd68f4ef76a0e02da78de3f84eb5a57846922224
Yucheng-An <yuc.an2019@gmail.com>
2024-12-05 09:44:54 -0800

    Commit Python

--- Geekforgeeks/leader.py
+++ Geekforgeeks/leader.py
@@ -2,7 +2,7 @@ def leaders(arr):
     initLeader = 0
     res = []
     for i in (0,len(arr)-1):
-        if arr[i]<arr[i+1]:
+        if arr[i]>arr[i+1]:
             initLeader = arr[i]
             res.append(initLeader)
     return res
