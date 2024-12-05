406ed987e8af5717a82651a4e7bd4d77f9e3630c
Yucheng-An <yuc.an2019@gmail.com>
2024-12-05 11:30:26 -0800

    Commit Python

--- Geekforgeeks/leader.py
+++ Geekforgeeks/leader.py
@@ -1,11 +1,13 @@
 def leaders(self, arr):
     res = []
     currentMax = float("-inf")
-    for i in range(len(arr)-1, -1, -1):
+    for i in range(len(arr) - 1, -1, -1):
         if arr[i] >= currentMax:
             res.append(arr[i])
             currentMax = arr[i]
     res.reverse()
     return res
+
+
 arr = [16, 17, 4, 3, 5, 2]
 print("Leaders:", leaders(arr))
