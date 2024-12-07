5ed146ce2806fdf4b5e4167239adf25993168930
Yucheng-An <yuc.an2019@gmail.com>
2024-12-07 01:57:08 -0800

    Commit Python

--- Geekforgeeks/kthSmallest.py
+++ Geekforgeeks/kthSmallest.py
@@ -3,8 +3,12 @@ def kthSmallest(arr,k):
     count = [0] * (maxElement+1)
     for i in arr:
         count[i] += 1
-    
-
+    count1 = 0
+    for i in range(len(count)):
+        count1 += count[i]
+        if count1 >= k:
+            return i
+    return -1
 testcase = [7,10,4,3,20,15]
 print(kthSmallest(testcase,2))
 print(testcase)
