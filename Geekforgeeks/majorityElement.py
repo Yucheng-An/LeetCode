0dac0ae17e95b8121b36d0db71e5dcfb43309c25
Yucheng-An <yuc.an2019@gmail.com>
2024-12-07 01:29:06 -0800

    Commit Python

---
+++ Geekforgeeks/majorityElement.py
@@ -0,0 +1,22 @@
+def majorityElement(arr):
+    candidate = None
+    count = 0
+    for i in arr:
+        if count == 0:
+            candidate = i
+            count += 1
+        elif i == candidate:
+            count += 1
+        else:
+            count -= 1
+    count = 0 
+    for i in arr:
+        if i == candidate:
+            count += 1
+    if count > len(arr):
+        return candidate
+    else:
+        return -1
+
+res = majorityElement([3,1,3,3,2])
+print(majorityElement([3,1,3,3,2]))
