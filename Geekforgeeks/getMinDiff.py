4709f1e1cdfbce93425a15cb1f9fef2499e0eb84
Yucheng-An <yuc.an2019@gmail.com>
2024-12-07 02:09:37 -0800

    Commit Python

--- Geekforgeeks/getMinDiff.py
+++ Geekforgeeks/getMinDiff.py
@@ -2,4 +2,7 @@ def getMinDiff(arr,k):
     arr.sort()
     for i in range(len(arr)):
         if i < len(arr)/2:
-            if arr[i]
+            arr[i] = arr[i] + k
+        else:
+            arr[i] = arr[i] - k
+    
