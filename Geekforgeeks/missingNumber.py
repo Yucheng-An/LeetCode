fe7e6707c312c131c4b67a54d93d7da9bf6c57b3
Yucheng-An <yuc.an2019@gmail.com>
2024-12-07 01:58:28 -0800

    Commit Python

---
+++ Geekforgeeks/missingNumber.py
@@ -0,0 +1,13 @@
+def missingNumber(self, arr):
+    sortedArr = arr.sort()
+    if len(arr) == 1:
+        if arr[0] == 1:
+            return arr[0]+1
+        else:
+            return arr[0]-1
+    if arr[0] != 1:
+        return 1
+    for i in range(1,len(arr)):
+        if arr[i] - arr[i-1] != 1:
+            return arr[i-1]+1
+    return arr[-1]+1
