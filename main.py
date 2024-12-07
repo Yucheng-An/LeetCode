bed2ab68b033edb683357ab922b57915d84db572
Yucheng-An <yuc.an2019@gmail.com>
2024-12-07 00:47:10 -0800

    Commit Python

--- main.py
+++ main.py
@@ -20,7 +20,6 @@
 def func(total,coins):
     dp = [0] * (total + 1)
     dp[0] = 1  # Base case: one way to make 0 cents
-
     for coin in coins:
         for x in range(coin, total + 1):
             dp[x] = dp[x] + dp[x - coin]
