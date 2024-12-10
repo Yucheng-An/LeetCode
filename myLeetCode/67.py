e2f4d09b0318450f32e754764bd6fc72afffe1bd
Yucheng-An <yuc.an2019@gmail.com>
2024-12-09 17:31:11 -0800

    Commit Python

--- myLeetCode/67.py
+++ myLeetCode/67.py
@@ -2,6 +2,8 @@ def addBinary(a,b):
     maxlengh = max(len(a),len(b))
     a = a.zfill(maxlengh)
     b = b.zfill(maxlengh)
+    carry = 0
+    res = []
     for i in range(maxlengh-1,-1,-1):
         
     
