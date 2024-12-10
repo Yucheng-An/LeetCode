2e87682e42c7e09a4f8731fc7bdb117ec978ad9c
Yucheng-An <yuc.an2019@gmail.com>
2024-12-09 17:29:41 -0800

    Commit Python

--- myLeetCode/67.py
+++ myLeetCode/67.py
@@ -1,14 +1,6 @@
 def addBinary(a,b):
-    t = ''
-    for i in range(abs(len(a)-len(b))):
-        t = t + '0'
-    if len(a)>len(b):
-        fullB = t + b
-        fullA = a
-    elif len(b) > len(a):
-        fullA = t + a
-        fullB = b
-
+    maxlengh = max(len(a),len(b))
+    
     
     
 
