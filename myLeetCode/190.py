d72c54456894d59d319508fd0f4f158d90aa8c4b
Yucheng-An <yuc.an2019@gmail.com>
2024-12-09 18:01:21 -0800

    Commit Python

--- myLeetCode/190.py
+++ myLeetCode/190.py
@@ -2,4 +2,8 @@ def reverseBits(n):
     result = 0 
     for i in range(32):
         result <<= 1
-        result |= (n$#@^&77)
+        result |= (n&1)
+        n>>=1
+    return result
+
+print(reverseBits())
