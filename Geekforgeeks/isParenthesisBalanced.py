d5f8c71450c7957d62267988c95292dad52f7d24
Yucheng-An <yuc.an2019@gmail.com>
2024-12-09 16:38:20 -0800

    Commit Python

---
+++ Geekforgeeks/isParenthesisBalanced.py
@@ -0,0 +1,26 @@
+def isParenthesisBalanced(self, s):
+    stack = []
+    for i in s:
+        if i == '{':
+            stack.append(i)
+        elif i == '(':
+            stack.append(i)
+        elif i == '[':
+            stack.append(i)
+        elif i == '{' and stack[-1] == '}':
+            stack.pop()
+        elif i == '(' and stack[-1] == ')':
+            stack.pop()
+        elif i == '[' and stack[-1] == ']':
+            stack.pop()
+        elif len(stack) == 0:
+            pass
+        else:
+            return False
+    if len(stack) == 0:
+        return True
+    else:
+        return False
+
+
+print(isParenthesisBalanced([]))
