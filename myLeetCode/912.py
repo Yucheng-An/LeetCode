b8879a2544a8b85a2b9da78b75938d085febaddf
Yucheng-An <yuc.an2019@gmail.com>
2024-12-11 00:51:39 -0800

    Commit Python

--- myLeetCode/912.py
+++ myLeetCode/912.py
@@ -1,29 +1,31 @@
 def myArray(self, nums):
     if len(nums) == 1:
         return nums
-    mid = len(nums)//2
+    mid = len(nums) // 2
     right = myArray(nums[:mid])
     left = myArray(nums[mid:])
-    return meger(right,left)
+    return meger(right, left)
 
-def sortArray(self, nums):
+
+def sortArray(nums):
     return myArray(nums)
 
-def meger(self, right,left):
+
+def meger(right, left):
     result = []
     i = 0
     j = 0
-    while i<len(left) and j < len(right):
-        if left[i]<right[j]:
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
             result.append(left[i])
             i += 1
         else:
             result.append(right[j])
-            j+=1
+            j += 1
     while i < len(left):
         result.append(left[i])
-        i+=1
+        i += 1
     while j < len(right):
         result.append(right[j])
-        j+=1
+        j += 1
     return result
