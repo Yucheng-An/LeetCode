fbe3332a0e19365c7998edc13988d43dbc7c02fa
Yucheng-An <yuc.an2019@gmail.com>
2024-12-16 17:57:35 -0800

    Commit Python

--- myLeetCode/118.py
+++ myLeetCode/118.py
@@ -4,7 +4,7 @@ def generate(numRows):
         subArray = []
         for j in range(0,i+1):
             if j != 0 or j != i:
-                
+                subArray.append(0)
         res.append(subArray)
     return res
 
