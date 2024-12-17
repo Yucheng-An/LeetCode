10ede112f232eef711f6014d443d277d6d19fcfe
Yucheng-An <yuc.an2019@gmail.com>
2024-12-16 19:24:45 -0800

    Commit Python

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ fruits = ['apple', 'banana', 'cherry','cherry','cherry','cherry']
 myMap = {}
 for index,value in enumerate(fruits):
     myMap[value] = index
-if 5 not in myMap:
+if "apple" not in myMap:
     print("here")
 print(myMap)
