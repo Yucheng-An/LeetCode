b10e105a24aceaccb053bb93242eef73f43fae96
Yucheng-An <yuc.an2019@gmail.com>
2024-12-16 17:56:43 -0800

    Commit Python

--- myLeetCode/118.py
+++ myLeetCode/118.py
@@ -2,7 +2,7 @@ def generate(numRows):
     res = []
     for i in range(numRows):
         subArray = []
-        for j in range(0,i):
+        for j in range(0,i+1):
             subArray.append(1)
         res.append(subArray)
     return res
