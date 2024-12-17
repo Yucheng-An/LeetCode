152d84994eb4af672554c903624d6adc098f4606
Yucheng-An <yuc.an2019@gmail.com>
2024-12-16 19:24:29 -0800

    Commit Python

--- main.py
+++ main.py
@@ -2,5 +2,6 @@ fruits = ['apple', 'banana', 'cherry','cherry','cherry','cherry']
 myMap = {}
 for index,value in enumerate(fruits):
     myMap[value] = index
-
+if 5 in myMap:
+    print("here")
 print(myMap)
