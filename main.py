84061f967d5de18f9a8638d42b831220b9d47b2c
Yucheng-An <yuc.an2019@gmail.com>
2024-12-17 14:59:49 -0800

    Commit Python

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ s = " hello world my name is something"
 t = s.split()
 print(t)
 res = ""
-for i in range(len(t)-2,0,-1):
+for i in range(len(t)-1,0,-1):
     res += t[i] + " "
 res += t[0]
 print(res)
