689be14c2fee9affe7825b402f4792b46f79c455
Yucheng-An <yuc.an2019@gmail.com>
2024-12-18 02:22:51 -0800

    Commit Python

--- myLeetCode/6.py
+++ myLeetCode/6.py
@@ -1,20 +1,9 @@
 def convert(s, numRows):
     if numRows == 1:
         return s
-    res = []
-    for i in range(numRows):
-        res.append([])
-    j = 0
-    for i in range(len(s)):
-        res[j].append(s[i])
-        j += 1
-        if j == numRows:
-            j = 0
-    finalres = ""
-    for i in res:
-        for j in i:
-            finalres += j
-    return finalres
+    rows = [''] * numRows 
+    current_row = 0
+    
 
 
 s = "PAYPALISHIRING"
