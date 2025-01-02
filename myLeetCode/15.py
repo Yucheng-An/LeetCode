ede83cd4988ec4e71b7cae4ff3b3243fd5ebed40
Yucheng-An <yuc.an2019@gmail.com>
2025-01-01 18:28:59 -0800

    Commit Python

--- myLeetCode/15.py
+++ myLeetCode/15.py
@@ -4,24 +4,23 @@ def threeSum(self, nums):
     for i in range(len(nums)):
         if nums[i] > 0:
             break
-        if i > 0 and nums[i] == nums[i-1]:
+        if i > 0 and nums[i] == nums[i - 1]:
             continue
-        pairs = helper(nums,i+1,-nums[i])
+        pairs = helper(nums, i + 1, -nums[i])
         for pair in pairs:
-            res.append([nums[i]]+pair)
+            res.append([nums[i]] + pair)
     return res
 
 
-
-def helper(nums,start,target):
+def helper(nums, start, target):
     pairs = []
     left = start
-    right = len(nums)-1
-    while left<right:
+    right = len(nums) - 1
+    while left < right:
         if target == nums[left] + nums[right]:
-            pairs.append([nums[left],nums[right]])
+            pairs.append([nums[left], nums[right]])
             left += 1
-            while left<right and nums[left] == nums[left-1]:
+            while left < right and nums[left] == nums[left - 1]:
                 left += 1
         elif target > nums[left] + nums[right]:
             left += 1
