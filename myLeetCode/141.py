48bbba68687f63c25550abf337cd1b44e52b7def
Yucheng-An <yuc.an2019@gmail.com>
2025-01-01 17:09:05 -0800

    Commit Python

---
+++ myLeetCode/141.py
@@ -0,0 +1,15 @@
+def hasCycle(self, head: Optional[ListNode]) -> bool:
+    f = head
+    s = head
+    while f is not None and f.next is not None:
+        f = f.next.next
+        s = s.next
+        if f == s:
+            break
+    if not f or not f.next:
+        return False
+    s = head
+    while s != f:
+        s = s.next
+        f = f.next
+    return True
