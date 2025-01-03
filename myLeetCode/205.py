cdc052a02c238f556530bec270a7745e98a4b9f1
Yucheng-An <yuc.an2019@gmail.com>
2025-01-03 00:16:13 -0800

    Commit Python

--- myLeetCode/205.py
+++ myLeetCode/205.py
@@ -29,19 +29,20 @@ def isIsomorphic(s: str, t: str) -> bool:
 
 s = "egg"
 t = "add"
-
-smap = {}
-tmap = {}
-for c in s:
-    if c not in smap:
-        smap[c] = 1
-    else:
-        smap[c] += 1
-for c in t:
-    if c not in tmap:
-        tmap[c] = 1
-    else:
-        tmap[c] += 1
-
-print(smap)
-print(tmap)
+isIsomorphic(s,t)
+# 
+# smap = {}
+# tmap = {}
+# for c in s:
+#     if c not in smap:
+#         smap[c] = 1
+#     else:
+#         smap[c] += 1
+# for c in t:
+#     if c not in tmap:
+#         tmap[c] = 1
+#     else:
+#         tmap[c] += 1
+# 
+# print(smap)
+# print(tmap)
