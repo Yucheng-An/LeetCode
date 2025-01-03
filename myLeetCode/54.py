383bd0bf8e5fed1d0b2e56825aa163d5fcacf730
Yucheng-An <yuc.an2019@gmail.com>
2025-01-03 00:54:36 -0800

    Commit Python

--- myLeetCode/54.py
+++ myLeetCode/54.py
@@ -25,4 +25,4 @@ print(helper(m))
 def helper1 (m,rowstart,rowend, columnstart, columnend):
     res = []
     for i in range(rowend-rowstart+1):
-        res.append(m[rowstart])
+        res.append(m[rowstart][])
