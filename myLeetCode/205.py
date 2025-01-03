c4cd26b26ccf5382d75a8f3fea71e9f4c5ebf4e8
Yucheng-An <yuc.an2019@gmail.com>
2025-01-03 00:15:49 -0800

    Commit Python

--- myLeetCode/205.py
+++ myLeetCode/205.py
@@ -1,9 +1,24 @@
-def isIsomorphic(s, t):
-    smap = {}
-    tmap = {}
-    pass
+def isIsomorphic(s: str, t: str) -> bool:
+    # Create dictionaries to store the mappings
+    map_s_to_t = {}
+    map_t_to_s = {}
 
+    for char_s, char_t in zip(s, t):
+        # Check s -> t mapping
+        if char_s in map_s_to_t:
+            if map_s_to_t[char_s] != char_t:
+                return False
+        else:
+            map_s_to_t[char_s] = char_t
 
+        # Check t -> s mapping
+        if char_t in map_t_to_s:
+            if map_t_to_s[char_t] != char_s:
+                return False
+        else:
+            map_t_to_s[char_t] = char_s
+
+    return True
 
 
 
