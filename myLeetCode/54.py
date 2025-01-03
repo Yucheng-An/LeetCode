dd027754bf036ff1cab4cab3079d484e5f6e2897
Yucheng-An <yuc.an2019@gmail.com>
2025-01-03 00:37:04 -0800

    Commit Python

--- myLeetCode/54.py
+++ myLeetCode/54.py
@@ -11,5 +11,5 @@ def helper(m):
         res.append(m[0][i])
     for i in range(1,len(m)):
         res.append(m[i][-1])
-    for i in range(len(m[0])):
+    for i in range(len(m[0])-1,0-1):
         res.append(m[-1][i])
