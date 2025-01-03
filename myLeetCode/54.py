437362a234cf17ed0649649d186abb785cf87d20
Yucheng-An <yuc.an2019@gmail.com>
2025-01-03 00:35:50 -0800

    Commit Python

--- myLeetCode/54.py
+++ myLeetCode/54.py
@@ -9,5 +9,5 @@ def helper(m):
     mrow = len(m[0])
     for i in range(len(m[0])):
         res.append(m[0][i])
-    for i in range(len(m)):
+    for i in range(1,len(m)):
         res.append(m[i])
