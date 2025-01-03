2732a3e6b7505406f275a52bc5b32d8014b9ab50
Yucheng-An <yuc.an2019@gmail.com>
2025-01-02 23:49:01 -0800

    Commit Python

--- myLeetCode/136.py
+++ myLeetCode/136.py
@@ -1,16 +1,7 @@
 def canCompleteCircuit(gas, cost):
-    totalgas = 0
-    totalcost = 0
-    currencost = 0
-    startstation = 0
-    for i in range(len(gas)):
-        totalgas += gas[i]
-        totalcost += cost[i]
-        currencost += gas[i] - cost[i]
-        if currencost < 0:
-            startstation = i + 1
-            currencost = 0
-    return startstation if totalgas > totalcost else -1
+    totalGas = 0
+    totalCost = 0 
+    
 
 
 gas = [1, 2, 3, 4, 5]
